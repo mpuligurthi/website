5eddb2e6c6af17f33d7402a42eba3d291670b45e
mpuligurthi <mpuligurthi@gmail.com>
2017-06-21 11:59:42 -0500

    typed.js welcome message working

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*Typed.js implementation for welcome message*/
+  document.addEventListener('DOMContentLoaded', function(){
+      Typed.new('.element', {
+        strings: ["Welcome!", "Bienvenidos!", "Bonjour!", "Willkommen!", "Benvenuto!", "नमस्ते", "ようこそ!"],
+        typeSpeed: 30
+      });
+  });
+
