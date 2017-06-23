caac86fb7afd03c31d6bc7a59c84887b59dcf387
mpuligurthi <mpuligurthi@gmail.com>
2017-06-23 12:23:23 -0500

    added more languages to welcome message

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /*Typed.js implementation for welcome message*/
   document.addEventListener('DOMContentLoaded', function(){
       Typed.new('.element', {
-        strings: ["Welcome!", "Bienvenidos!", "Bonjour!", "Willkommen!", "Benvenuto!", "नमस्ते", "ようこそ!"],
-        typeSpeed: 30
+        strings: ["Welcome!", "Bienvenidos!", "సుస్వాగతం", "Bonjour!", "Bem-vindos!", "Willkommen!", "Benvenuto!", "नमस्ते", "ようこそ!"],
+        typeSpeed: 40
       });
   });
 
