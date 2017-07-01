9611c7bc7645c58871738f17acb79eb2263a9899
mpuligurthi <mpuligurthi@gmail.com>
2017-06-30 20:54:54 -0500

    started travels section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*Typed.js implementation for welcome message*/
-  document.addEventListener('DOMContentLoaded', function(){
+  document.addEventListener('DOMContentLoaded', function() {
       Typed.new('.element', {
-        strings: ["Welcome!", "Bienvenidos!", "సుస్వాగతం", "नमस्ते", "خوش آمدید", "Bonjour!", "Bem-vindos!", "Willkommen!", "Benvenuto!", "ようこそ!", "欢迎"],
+        strings: ["Welcome!", "Bienvenidos!", "సుస్వాగతం", "नमस्ते", "خوش آمدید", "Bonjour!", "Bem-vindos!", "Willkommen!", "Benvenuto!", "ようこそ!", "欢迎!"],
         typeSpeed: 40
       });
   });
